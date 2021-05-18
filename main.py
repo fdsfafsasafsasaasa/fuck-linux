a271c80e462426630606fad8865f2bf37d684647
lexi <lexi@debian.home>
2021-05-17 22:05:16 -0500

    fuck linux

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import typing
+import progressbar
+import ctypes 
+from time import sleep
+from progressbar.progressbar import ProgressBar
+
+class Pakt_Init: # putting it in class because oop autism
+    # white space because clean code
+    @staticmethod # static method because this shouldn't be in a class, but read above lines
+    def pakt_init(name: typing.Optional[str], boot: bool = True) -> None: # setting parameters and return types
+        if not isinstance(name, str): # checking if the name provided is a string or not, type hinting doesn't raise errors iirc
+            raise TypeError("`Name` has to be a string.") # raising error if they messed up
+        # white space because clean code
+        if not isinstance(boot, bool): # checking if boot is a boolean or not
+            raise TypeError("`Boot` has to be a boolean.") # raising error
+        # white space because clean code
+        os.system("clear") # clearing screen
+        # more white space because clean code
+        print("Initializing system")
+        # more white space because clean code
+        pakt_init_progress_bar = ProgressBar(maxval=100) # creating instance of `ProgressBar`
+        pakt_init_progress_bar.start() # starting instance of `ProgressBar`
+        # more white space because clean code 
+        for i in range(0, 100): # creating loop to iterate over i
+            if not isinstance(i, int): # type checking
+                raise TypeError("Iterator variable `i` must be an integer in this context.") # raising error if type check fails
+            # more white space because clean code
+            pakt_init_progress_bar.update(i+1) # updating progress bar
+            sleep(0.025)
+        pakt_init_progress_bar.finish() # finish progress bar (same with ur mother)
+        # more white space because clean code 
+        print("Pakt Init is done initializing the system.") # we are done
+        ctypes.string_at(0) # segfault python because FUCK YOU
+        
+        
+pakt_init_object = Pakt_Init()
+
+pakt_init_object.pakt_init("penis", True)
